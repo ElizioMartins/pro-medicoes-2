7fed80ad264998771ef591c97bd3a232ef097276
Elizio Martins Cezarino <78484991+ElizioMartins@users.noreply.github.com>
2025-06-10 11:45:46 -0300

    Ajusta lista de condominios

--- frontend/src/app/core/models/Condominium.ts
+++ frontend/src/app/core/models/Condominium.ts
@@ -1,5 +1,7 @@
 export interface Condominium {
   id: string;
   name: string;
-  address?: string; // Optional: address or other relevant details
+  address: string;
+  unitsCount?: number;
+  metersCount?: number;
 }
