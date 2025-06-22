77c84b29db47029fb52b7554d5849a30c5e42baf
Elizio Martins Cezarino <78484991+ElizioMartins@users.noreply.github.com>
2025-06-22 08:44:38 -0300

    ajuste

--- frontend/src/app/core/services/user.service.ts
+++ frontend/src/app/core/services/user.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { User } from '@core/models/User';
 import { environment } from '../../../environments/environment';
+import { User } from '@app/shared/models/user.model';
 
 @Injectable({
   providedIn: 'root'
