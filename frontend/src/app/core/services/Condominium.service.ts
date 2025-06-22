dffed9ed3391c4783f2d5ac29fb1151dde41d6fa
Elizio Martins Cezarino <78484991+ElizioMartins@users.noreply.github.com>
2025-06-22 09:00:20 -0300

    comit

--- frontend/src/app/core/services/Condominium.service.ts
+++ frontend/src/app/core/services/Condominium.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Condominium, CondominiumCreate, CondominiumUpdate } from '../../shared/models/condominium.model';
 import { BaseApiService } from './base-api.service';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { PaginatedResponse } from '../../shared/models/api-response.model';
 import { Unit } from '../../shared/models/unit.model';
 
@@ -22,6 +22,39 @@ export class CondominiumService extends BaseApiService<Condominium, CondominiumC
 
   getStatistics(condominiumId: number): Observable<any> {
     return this.http.get<any>(`${this.baseUrl}/${condominiumId}/statistics`);
+  }
+
+  getCondominiumById(condominiumId: number): Observable<Condominium> {
+    return this.http.get<Condominium>(`${this.baseUrl}/${condominiumId}`)
+      .pipe(
+        map(condominium => ({
+          ...condominium,
+          id: Number(condominiumId) // Garante que o ID é um número
+        }))
+      );
+  }
+   createCondominium(condominium: Partial<Condominium>): Observable<Condominium> {
+    return this.http.post<Condominium>(this.baseUrl, condominium)
+      .pipe(
+        map(condominium => ({
+          ...condominium,
+          id: Number(condominium.id) // Garante que o ID é um número
+        }))
+      );
+  }
+
+  updateCondominium(condominiumId: number, condominium: Partial<Condominium>): Observable<Condominium> {
+    return this.http.put<Condominium>(`${this.baseUrl}/${condominiumId}`, condominium)
+      .pipe(
+        map(condominium => ({
+          ...condominium,
+          id: Number(condominium.id) // Garante que o ID é um número
+        }))
+      );
+  }
+
+  deleteCondominium(condominiumId: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${condominiumId}`);
   }
 }
 
